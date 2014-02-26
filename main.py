f1de245be729832d40e622b9f9dce5d301a87d90
Nateliason <nateliason@gmail.com>
2014-02-26 16:06:46 -0500

    Functional, but not clean yet

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from credentials import *
 import twitter
 
-def gettimeline():
-	api = twitter.Api(consumer_key=myconsumerkey,
+
+api = twitter.Api(consumer_key=myconsumerkey,
 		consumer_secret=myconsumersecret,
 		access_token_key=myaccesstokenkey,
 		access_token_secret=myaccesstokensecret)
-	print [i.text for i in api.GetUserTimeline(count=200)]
 
-gettimeline()
+
+def gettimeline():
+	return api.GetUserTimeline(count=200)
+
+def clean_timeline(timeline):
+	for i in timeline:
+		if 'bad_word' in i.text:
+			api.DestroyStatus(i.id)
+			print "Just destroyed %d" % i.text
+
+clean_timeline(gettimeline())
